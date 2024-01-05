a55a2ac7968c2a6bc1f6b0d044e8e7e1bd706506
RomanKrivocheev <roman.krivocheev@gmail.com>
2024-01-05 02:16:44 +0100

    Working basic pagination

--- dist/frontend/src/App.js
+++ dist/frontend/src/App.js
@@ -2,21 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 const react_1 = require("react");
-function App() {
+const App = () => {
     const [tracks, setTracks] = (0, react_1.useState)([]);
     const [page, setPage] = (0, react_1.useState)(0);
-    const [loading, setLoading] = (0, react_1.useState)(true);
-    (0, react_1.useEffect)(() => {
-        const fetchData = async () => {
-            try {
-                console.log('Fetching data for page:', page);
-                const response = await fetch('http://localhost:3000/graphql', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        query: `
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/graphql', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    query: `
               query GetTracks($input: GetTracksInput!) {
                 getTracks(input: $input) {
                   id
@@ -27,38 +24,32 @@ function App() {
                 }
               }
             `,
-                        variables: {
-                            input: {
-                                artistName: 'AC/DC',
-                                genreName: 'Rock',
-                                minPrice: 0,
-                                maxPrice: 1,
-                                page,
-                                pageSize: 5,
-                            },
+                    variables: {
+                        input: {
+                            artistName: 'AC/DC',
+                            genreName: 'Rock',
+                            minPrice: 0,
+                            maxPrice: 100,
+                            page,
+                            pageSize: 2,
                         },
-                    }),
-                });
-                const result = await response.json();
-                if (result.data.getTracks.length > 0) {
-                    setTracks((prevTracks) => [
-                        ...prevTracks,
-                        ...result.data.getTracks,
-                    ]);
-                    setPage((prevPage) => prevPage + 1);
-                }
-                setLoading(false);
+                    },
+                }),
+            });
+            const result = await response.json();
+            if (result.data.getTracks.length > 0) {
+                setTracks((prevTracks) => [
+                    ...prevTracks,
+                    ...result.data.getTracks,
+                ]);
+                setPage((prevPage) => prevPage + 1);
             }
-            catch (error) {
-                console.error('Error fetching tracks:', error);
-                setLoading(false);
-            }
-        };
-        if (loading && tracks.length === 0) {
-            fetchData();
         }
-    }, [loading, page, tracks]);
-    return ((0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "App" }, { children: [(0, jsx_runtime_1.jsx)("h1", { children: "Paginated Track List" }), "Page: ", page, loading && (0, jsx_runtime_1.jsx)("p", { children: "Loading..." }), !loading && ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [tracks.map((track, index) => ((0, jsx_runtime_1.jsxs)("div", { children: [(0, jsx_runtime_1.jsx)("h3", { children: track.name }), (0, jsx_runtime_1.jsxs)("p", { children: ["Price: ", track.price] }), (0, jsx_runtime_1.jsxs)("p", { children: ["Genre: ", track.genre] }), (0, jsx_runtime_1.jsx)("hr", {})] }, `${track.id}-${index}`))), (0, jsx_runtime_1.jsx)("button", Object.assign({ onClick: () => setPage((prevPage) => prevPage + 1) }, { children: "Load More" }))] }))] })));
-}
+        catch (error) {
+            console.error('Error fetching tracks:', error);
+        }
+    };
+    return ((0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "App" }, { children: [(0, jsx_runtime_1.jsx)("h1", { children: "Paginated Track List" }), "Page: ", page, (0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [tracks.map((track, index) => ((0, jsx_runtime_1.jsxs)("div", { children: [(0, jsx_runtime_1.jsx)("h3", { children: track.name }), (0, jsx_runtime_1.jsxs)("p", { children: ["Price: ", track.price] }), (0, jsx_runtime_1.jsxs)("p", { children: ["Genre: ", track.genre] }), (0, jsx_runtime_1.jsx)("hr", {})] }, `${track.id}-${index}`))), (0, jsx_runtime_1.jsx)("button", Object.assign({ onClick: () => fetchData() }, { children: "Load More" }))] })] })));
+};
 exports.default = App;
 //# sourceMappingURL=App.js.map
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Track } from '../../src/track.model';
 
-function App() {
+const App = () => {
   const [tracks, setTracks] = useState<Track[]>([]);
   const [page, setPage] = useState(0);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        console.log('Fetching data for page:', page);
-
-        const response = await fetch('http://localhost:3000/graphql', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            query: `
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `
               query GetTracks($input: GetTracksInput!) {
                 getTracks(input: $input) {
                   id
@@ -28,62 +24,50 @@ function App() {
                 }
               }
             `,
-            variables: {
-              input: {
-                artistName: 'AC/DC',
-                genreName: 'Rock',
-                minPrice: 0,
-                maxPrice: 1,
-                page,
-                pageSize: 5,
-              },
+          variables: {
+            input: {
+              artistName: 'AC/DC',
+              genreName: 'Rock',
+              minPrice: 0,
+              maxPrice: 100,
+              page,
+              pageSize: 2,
             },
-          }),
-        });
+          },
+        }),
+      });
 
-        const result = await response.json();
+      const result = await response.json();
 
-        if (result.data.getTracks.length > 0) {
-          setTracks((prevTracks) => [
-            ...prevTracks,
-            ...(result.data.getTracks as Track[]),
-          ]);
-          setPage((prevPage) => prevPage + 1);
-        }
-
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching tracks:', error);
-        setLoading(false);
+      if (result.data.getTracks.length > 0) {
+        setTracks((prevTracks) => [
+          ...prevTracks,
+          ...(result.data.getTracks as Track[]),
+        ]);
+        setPage((prevPage) => prevPage + 1);
       }
-    };
-    if (loading && tracks.length === 0) {
-      fetchData();
+    } catch (error) {
+      console.error('Error fetching tracks:', error);
     }
-  }, [loading, page, tracks]);
+  };
 
   return (
     <div className="App">
       <h1>Paginated Track List</h1>
       Page: {page}
-      {loading && <p>Loading...</p>}
-      {!loading && (
-        <>
-          {tracks.map((track, index) => (
-            <div key={`${track.id}-${index}`}>
-              <h3>{track.name}</h3>
-              <p>Price: {track.price}</p>
-              <p>Genre: {track.genre}</p>
-              <hr />
-            </div>
-          ))}
-          <button onClick={() => setPage((prevPage) => prevPage + 1)}>
-            Load More
-          </button>
-        </>
-      )}
+      <>
+        {tracks.map((track, index) => (
+          <div key={`${track.id}-${index}`}>
+            <h3>{track.name}</h3>
+            <p>Price: {track.price}</p>
+            <p>Genre: {track.genre}</p>
+            <hr/>
+          </div>
+        ))}
+        <button onClick={() => fetchData()}>Load More</button>
+      </>
     </div>
   );
-}
+};
 
 export default App;
